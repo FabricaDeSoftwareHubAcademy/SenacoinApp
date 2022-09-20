46586de97feb18e4c242a26937c759c4a89e0d8f
Gaikko <Gaikko@gmail.com>
2022-09-20 10:42:27 -0400

    1

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, TouchableOpacity, Text, TextInput, View } from 'react-native';
+import logo from './assets/galaxia.png';
+import MenuLateral from './components/MenuLateral';
+import PizzaTranslator from './components/PizzaTranslator';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Novo Texto</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
       <StatusBar style="auto" />
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>Pick a photo</Text>
+      </TouchableOpacity>
+      <TextInput placeholder='Escrever' style={styles.TextInput}></TextInput>
+      <PizzaTranslator/>
+      <MenuLateral style={styles.TextInputML}/>
     </View>
   );
 }
@@ -16,5 +30,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  TextInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 15
   },
 });
---
+++ components/MenuLateral.js
@@ -0,0 +1,21 @@
+import {Text, TextInput, View, StyleSheet} from 'react-native';
+
+const MenuLateral = () => {
+    return (
+        <View>
+            <Text>Teste Componetes</Text>
+            <TextInput placeholder='Olá' style={styles1.TextInputML}/>
+        </View>
+    );
+}
+
+const styles1 = StyleSheet.create({
+    TextInputML: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      borderRadius: 15
+    },
+  });
+
+export default MenuLateral;
